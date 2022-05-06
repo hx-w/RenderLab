5f0e63761362f980319c1e182f42748fc5bac41b
hx-w <herixth@163.com>
2022-05-06 13:16:36 +0800

    issue fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "libs/coords.h"
+#include <coords.h>
 
 using namespace std;
 
 int main() {
     Coords pt(1.2, 2.0, 3.0);
-    cout << pt.normalize() << endl;
+    cout << pt.hash() << endl;
     return 0;
 }

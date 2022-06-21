834a991462d7c93721cfd02d5f3ed4524815ac99
hx-w <herixth@163.com>
2022-06-21 20:15:47 +0800

    usable background

--- resource/shader/background.fs
+++ resource/shader/background.fs
@@ -2,19 +2,21 @@
 
 in vec3 FragPos;
 
+uniform vec3 viewPos;
+
 out vec4 FragColor;
 
 void main() {
-	float scale = 5.0f;
+	float scale = 0.5f;
 
 	int c = (
-		int(round(FragPos.x * 10.0)) +
-		int(round(FragPos.z * 10.0))
+		int(round(FragPos.x * scale)) +
+		int(round(FragPos.z * scale))
 	) % 2;
 
 	if (c == 0) {
-		FragColor = vec4(0.3f, 0.3f, 0.3f, 1.0f);
+		FragColor = vec4(0.3f, 0.3f, 0.3f, 0.8f);
 	} else {
-		FragColor = vec4(0.8f, 0.8f, 0.8f, 1.0f);
+		FragColor = vec4(0.8f, 0.8f, 0.8f, 0.8f);
 	}
 }
--- resource/shader/background.vs
+++ resource/shader/background.vs
@@ -5,12 +5,14 @@ layout (location = 0) in vec3 aPos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 viewPos;
 
 out vec3 FragPos; 
 out vec3 near;
 out vec3 far;
 
 void main() {
-    FragPos = aPos.xyz * 50.0f;
+    FragPos.xz = (aPos.xz * 200.0f) + viewPos.xz;
+    FragPos.y = 0.0f;
     gl_Position = projection * view * vec4(FragPos, 1.0f);
 }

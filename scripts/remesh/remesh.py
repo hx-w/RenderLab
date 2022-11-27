21c0e10526e7d561ba0615f93b829f8e92a5cf53
CarOL <herixth@163.com>
2022-11-27 13:40:53 +0800

    fix boundary-fix mapping

--- scripts/remesh/remesh.py
+++ scripts/remesh/remesh.py
@@ -9,11 +9,12 @@ def convert(inp: str, otp: str, pivots: list):
     mesh = trimesh.load(inp)
 
     # remesh mesh
-    str_mesh = remesh_mesh(mesh, pivots)
+    str_mesh, param_msh = remesh_mesh(mesh, pivots)
 
     print(str_mesh, 'save to', otp)
     # save mesh
     str_mesh.export(otp)
+    param_msh.export(otp.replace('remesh', 'param'))
 
 
 def remesh_mesh(uns_msh: trimesh.Trimesh, pivots: list) -> trimesh.Trimesh: 
@@ -25,10 +26,10 @@ def remesh_mesh(uns_msh: trimesh.Trimesh, pivots: list) -> trimesh.Trimesh:
     print('pivots: ', pivots)
     # parameterize boundary
     bnd_verts = bnd_verts[1:] # first == last
-    f_B = mapping_boundary(uns_msh, bnd_verts, pivots)
+    f_B, bnd_verts = mapping_boundary(uns_msh, bnd_verts, pivots)
     sp_weights = initialize_weights(uns_msh, inn_verts, bnd_verts)
     # parameterize interior
     f_I = solve_equation(sp_weights, f_B, inn_verts, bnd_verts)
     param_msh = build_param_mesh(uns_msh, inn_verts, bnd_verts, f_I, f_B)
     str_msh = build_str_mesh(uns_msh, param_msh, 100)
-    return str_msh
+    return str_msh, param_msh
